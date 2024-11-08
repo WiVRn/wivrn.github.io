b306e577b01948098b5b97d028d715c64e101d48
Sergio Navarro <38010679+Respuit@users.noreply.github.com>
2024-11-08 21:08:46 +0100

    Settings guide (#1)
    * Settings guide
    
    * added FAQ
    
    * various fixes
    
    * Fixed typo and bitrate limits

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,36 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+    document.querySelectorAll("pre").forEach(preTag => {
+
+        const copyButton = document.createElement("span");
+        copyButton.innerText = "Copy";
+        copyButton.classList.add("copy-button");
+
+        preTag.appendChild(copyButton);
+
+        copyButton.addEventListener("click", () => {
+            copyButton.style.display = "none";
+
+            const range = document.createRange();
+            range.selectNode(preTag);
+            window.getSelection().removeAllRanges();
+            window.getSelection().addRange(range);
+
+            try {
+                document.execCommand("copy");
+
+                copyButton.innerText = "Copied!";
+                setTimeout(function() {
+                    copyButton.innerText = "Copy";
+                }, 2000);
+            } catch (err) {
+                console.error("Unable to copy text:", err);
+            } finally {
+                copyButton.style.display = "inline";
+                window.getSelection().removeAllRanges();
+            }
+        });
+    });
